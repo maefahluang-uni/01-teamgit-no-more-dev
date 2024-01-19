2dc4b1d83e1fd46b88b9760be87b4b2dad5272dd
Alicha112 <98002048+Alicha112@users.noreply.github.com>
2024-01-19 08:20:30 +0000

    Dev1 F1

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -30,12 +30,16 @@ public class Counter {
 
 	// TODO: dev1- method for increment to closest even number
 	public void incrementToEven() {
-		_ctr = -99;
+		 if (_ctr % 2 != 0) {
+        _ctr++; // Increment to make it even
+    }
 	}
 
 	// TODO: dev1- method for decrement to closest even number
 	public void decrementToEven() {
-		_ctr = -99;
+
+        _ctr -= (_ctr %2 ==0)?2:1; // Decrement to make it even
+    
 	}
 
 	// TODO: dev2- method for increment to closest prime number
